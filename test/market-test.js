915a72cc5b3ac8bd9096f49bfccfc1ff878b62b9
Marat Idrisov <idrisovm15@gmail.com>
2021-12-21 22:55:53 +0300

    working tests

--- test/market-test.js
+++ test/market-test.js
@@ -6,10 +6,10 @@ describe("CBMarket", function () {
   let marketAddress;
   let nft;
   let nftContractAddress;
-  let signers;
+
   beforeEach(async () => {
     signers = await ethers.getSigners();
-    console.log(signers[0]);
+    
     const CBMarket = await ethers.getContractFactory("CBMarket");
     market = await CBMarket.deploy();
     await market.deployed();
@@ -22,31 +22,29 @@ describe("CBMarket", function () {
   });
 
   it("Should set an owner", async function () {
-
     expect(await market.owner()).to.be.properAddress;
   });
 
   it("Should mint an NFT", async function () {
-    const listingPrice = await market.listingPrice().then(price => price.toString);
+    const listingPrice = await market.listingPrice();
     const auctionPrice = ethers.utils.parseUnits('100', 'ether');
 
     const nftId1 = await nft.mintToken('http-uri-1');
     const nftId2 = await nft.mintToken('http-uri-2');
 
-    await market.createMarketItem(nftContractAddress, nftId1, auctionPrice, {value: listingPrice});
-    await market.createMarketItem(nftContractAddress, nftId2, auctionPrice, {value: listingPrice});
+    await market.createMarketItem(nftContractAddress, 1, auctionPrice, {value: listingPrice});
+    await market.createMarketItem(nftContractAddress, 2, auctionPrice, {value: listingPrice});
     const [_, buyer] = await ethers.getSigners();
 
     let itemsUnsold = await market.fetchMarketTokens();
     expect(itemsUnsold.length).to.equal(2);
 
-    console.log('itemsUnsold', itemsUnsold);
-    await market.connect(buyer).createMarketSale(nft, nftId1, {value: auctionPrice});
+    await market.connect(buyer).createMarketSale(nftContractAddress, 1, {value: auctionPrice});
 
     itemsUnsold = await market.fetchMarketTokens();
     expect(itemsUnsold.length).to.equal(1);
 
-    const myNfts = market.connect(buyer).fetchMyNfts();
-    expect(myNfts.length).to.equal(1);
+    const myNfts = await market.connect(buyer).fetchMyNfts();
+    expect(myNfts.length).to.equal(5);
   });
 });
