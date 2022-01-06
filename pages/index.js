1ba781a69f148ff2e8efb35f7a2a97fca8939733
Marat Idrisov <idrisovm15@gmail.com>
2022-01-06 21:04:39 +0300

    form minting added blyat

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-
-import {nftAddress, marketAddress} from '../config'
+import { useEffect, useState } from 'react'
+import Web3Modal from "web3modal";
+import { ethers } from 'ethers'
+import axios from 'axios'
+import { Card } from 'antd';
+import { nftAddress, marketAddress } from '../config'
 
 import CBMarket from '../artifacts/contracts/CBMarket.sol/CBMarket.json'
 import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
 
 export default function Home() {
 
@@ -18,23 +20,67 @@ export default function Home() {
   async function loadNfts() {
     setLoading(true)
     const provider = new ethers.providers.JsonRpcProvider()
-    const tokenContract = new ethers.Contract(nftAddress, NFT.abi, provider)
+    const nftContract = new ethers.Contract(nftAddress, NFT.abi, provider)
     const marketContract = new ethers.Contract(marketAddress, CBMarket.abi, provider)
     const data = await marketContract.fetchMarketTokens()
     console.log(data);
-    const items = data.filter(i => typeof i === 'object' && i.sold === false).map(i => ({
-      itemId: i.itemId.toNumber(),
-      price: ethers.utils.formatUnits(i.price, 'ether'),
-      seller: i.seller
-    }));
+    const items = await Promise.all(data
+      .filter(i => typeof i === 'object' && i.sold === false)
+      .map(async i => {
+        const tokenUri = await nftContract.tokenURI(i.tokenId);
+        const meta = await axios.get(tokenUri);
+        const { image, name, description } = meta.data;
+        const price = ethers.utils.formatUnits(i.price, 'ether');
+        return {
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: owner,
+          image,
+          name,
+          description,
+          price
+        }
+      }));
     setNfts(items)
+    setLoading(false)
+  }
+
+  async function buyNft(nft) {
+    const web3 = new Web3Modal()
+    const connection = await web3.connect()
+    const provider = web3.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+    const marketContract = new ethers.Contract(marketAddress, CBMarket.abi, signer)
+
+    const price = ethers.utils.parseEther(nft.price);
+    const transaction = await marketContract(nftAddress, nft.tokenId);
+    await transaction.wait()
+    loadNfts()
   }
 
   return (
     <div>
       Hello!
-      {loading ? 'Loaded' : 'loading'}
-      {nfts.map(nft => <div>price: {nft.price}, itemId: {nft.itemId}, seller: {nft.seller}. </div>)}
+      {loading ? 'Loading' : 'Loaded'}
+      {nfts.map(nft => <Card
+        key={nft.tokenId}
+        style={{ width: 300 }}
+        cover={
+          <img
+            alt="nft image"
+            src={nft.image}
+          />
+        }
+        actions={[
+          
+        ]}
+      >
+        <Card.Meta
+          title={nft.name + ' ' + nft.price + 'ETH'}
+          description={nft.description}
+        />
+      </Card>
+      )}
     </div>
   )
 }
